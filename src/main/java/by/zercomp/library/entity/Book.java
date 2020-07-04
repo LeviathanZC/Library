e5870d9ed2ab6574deaa9518440fb8b1d49d22ec
LeviathanZC <leviathan.zercomp@gmail.com>
2020-07-04 16:26:12 +0300

    Add hashCode() method to Book Entity.

--- src/main/java/by/zercomp/library/entity/Book.java
+++ src/main/java/by/zercomp/library/entity/Book.java
@@ -1,8 +1,8 @@
 package by.zercomp.library.entity;
 
 public class Book {
-    private int id;
 
+    private int id;
     private String title;
     private String[] authors;
     private String publisher;
@@ -14,5 +14,15 @@ public class Book {
         this.publisher = publisher;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 113;
+        int result = id;
+        for (String item : authors) {
+            result += item.hashCode() * prime;
+        }
+        result += (title.hashCode() + publisher.hashCode()) * prime;
+        return result;
+    }
 
 }
