4f93e1bef37068393afe33ffd89b77fece0117c8
LeviathanZC <leviathan.zercomp@gmail.com>
2020-07-04 23:57:48 +0300

    Described interface LibraryService.

--- src/main/java/by/zercomp/library/service/LibraryService.java
+++ src/main/java/by/zercomp/library/service/LibraryService.java
@@ -1,4 +1,17 @@
 package by.zercomp.library.service;
 
+import by.zercomp.library.entity.Book;
+
+import java.util.List;
+
 public interface LibraryService {
+
+    void addNewBook(Book book);
+    Book getBookById(int id);
+    List<Book> getAllBooks();
+    void replaceBook(Book book);
+    void removeBook(Book book);
+    void removeBookAtId(int id);
+    void removeAllBooks();
+
 }
