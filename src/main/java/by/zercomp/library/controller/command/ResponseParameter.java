b093e8c108081360b6b9402ebe8948db70eb6ca2
LeviathanZC <leviathan.zercomp@gmail.com>
2020-07-19 23:15:01 +0300

    Describe ResponseParameter.

--- src/main/java/by/zercomp/library/controller/command/ResponseParameter.java
+++ src/main/java/by/zercomp/library/controller/command/ResponseParameter.java
@@ -3,5 +3,10 @@ package by.zercomp.library.controller.command;
 public class ResponseParameter {
 
     public static final String STATUS = "status";
+    public static final String STATUS_FAIL = "fail";
+    public static final String STATUS_SUCCESS = "success";
+    public static final String RESULT = "result";
 
+    private ResponseParameter() {
+    }
 }
