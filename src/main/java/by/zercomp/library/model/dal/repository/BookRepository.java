8ac983bcb4f49f9fe9af51a385a8300fdab57207
LeviathanZC <leviathan.zercomp@gmail.com>
2020-07-19 23:53:11 +0300

    Create CD methods for dataContext at BookRepository.

--- src/main/java/by/zercomp/library/model/dal/repository/BookRepository.java
+++ src/main/java/by/zercomp/library/model/dal/repository/BookRepository.java
@@ -25,11 +25,30 @@ public class BookRepository {
     /**
      * Метод для получения контекста данных.
      * Необходим для предотвращения изменения данных в данном контексте.
+     *
      * @return dataContext - list of books.
      */
     public List<Book> getDataContext() {
         return Collections.unmodifiableList(dataContext);
     }
 
+    public void add(Book book) {
+        if(dataContext.isEmpty()) {
+            book.setId(1);
+        } else {
+            Book temp = dataContext.get(dataContext.size() - 1);
+            book.setId(temp.getID() + 1);
+        }
+        dataContext.add(book);
+    }
+
+    public void remove(Book book) {
+        final int bookId = book.getID();
+        for (Book item : dataContext) {
+            if(item.getID() == bookId) {
+                dataContext.remove(item);
+            }
+        }
+    }
 
 }
