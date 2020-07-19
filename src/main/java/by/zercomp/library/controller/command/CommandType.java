a6e510592cf4e5541e9fcbcc7ca0f6c31382782d
LeviathanZC <leviathan.zercomp@gmail.com>
2020-07-19 23:08:31 +0300

    Add command field to the enum CommandType.

--- src/main/java/by/zercomp/library/controller/command/CommandType.java
+++ src/main/java/by/zercomp/library/controller/command/CommandType.java
@@ -3,5 +3,13 @@ package by.zercomp.library.controller.command;
 public enum CommandType {
 
     ADD_BOOK, REMOVE_BOOK, FIND_BOOK, SORT_BOOK;
+    private Command command;
 
+    CommandType(Command command) {
+        this.command = command;
+    }
+
+    public Command getCommand(){
+        return this.command;
+    }
 }
