520f024d121b4e8edd132e2df1f7557e922642e1
LeviathanZC <leviathan.zercomp@gmail.com>
2020-07-20 03:55:09 +0300

    Create and impl FindByPublisherCommandImpl.

---
+++ src/main/java/by/zercomp/library/controller/command/impl/FindByPublisherCommandImpl.java
@@ -0,0 +1,31 @@
+package by.zercomp.library.controller.command.impl;
+
+import by.zercomp.library.controller.command.Command;
+import by.zercomp.library.controller.command.parameter.RequestParameter;
+import by.zercomp.library.controller.command.parameter.ResponseParameter;
+import by.zercomp.library.model.entity.Book;
+import by.zercomp.library.model.exception.InvalidModelException;
+import by.zercomp.library.model.service.impl.LibraryServiceImpl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindByPublisherCommandImpl implements Command {
+    @Override
+    public Map<String, Object> execute(Map<String, Object> params) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            List<Book> found = LibraryServiceImpl.getService().findBookByPublisher(
+                    (String) params.get(RequestParameter.PUBLISHER)
+            );
+            response.put(ResponseParameter.STATUS, ResponseParameter.STATUS_SUCCESS);
+            for (Book item : found) {
+                response.put(ResponseParameter.RESULT, item);
+            }
+        } catch (InvalidModelException ex) {
+            response.put(ResponseParameter.STATUS, ResponseParameter.STATUS_FAIL);
+        }
+        return response;
+    }
+}
