46150c06aa8cffd45411da39496e4fe8b746d5c8
LeviathanZC <leviathan.zercomp@gmail.com>
2020-07-20 04:12:23 +0300

    Create RequestHandler.

--- src/main/java/by/zercomp/library/controller/invoker/RequestHandler.java
+++ src/main/java/by/zercomp/library/controller/invoker/RequestHandler.java
@@ -1,7 +1,15 @@
 package by.zercomp.library.controller.invoker;
 
+import by.zercomp.library.controller.command.Command;
+import by.zercomp.library.controller.command.definer.CommandDefiner;
+
+import java.util.Map;
+
 public class RequestHandler {
 
-
+    public Map<String, Object> runRequest(String action, Map<String, Object> params) {
+        Command command = CommandDefiner.define(action);
+        return command.execute(params);
+    }
 
 }
