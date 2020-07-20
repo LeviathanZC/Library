d8f311786691fbbae43ddf802fb2ef1b9cdc9bb2
LeviathanZC <leviathan.zercomp@gmail.com>
2020-07-20 16:31:11 +0300

    Make JDBC work fine.

--- src/main/java/by/zercomp/library/controller/MainController.java
+++ src/main/java/by/zercomp/library/controller/MainController.java
@@ -1,8 +1,15 @@
 package by.zercomp.library.controller;
 
+import by.zercomp.library.model.exception.InvalidModelException;
+import by.zercomp.library.model.factory.HibernateSessionFactoryUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MainController {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InvalidModelException {
+        HibernateSessionFactoryUtil.getSessionFactory();
     }
 }
